3819720321210332152ecadf23b05e76fe99b94f
levelp <onojasteven13@gmail.com>
2025-10-13 13:44:09 +0100

    work

--- provider.tf
+++ provider.tf
@@ -1,13 +1,10 @@
-# provider "aws" {
-#   region  = "eu-west-2"
-#   # profile = "pet-adoption"
-# }
 
 terraform {
   backend "s3" {
     bucket       = "steven-auto-discovery"
     // use_lockfile = true
     key          = "infrastructure/terraform.tfstate"
+    # key     = "env:/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
     # profile      = "pet-adoption"
@@ -17,4 +14,8 @@ terraform {
 provider "vault" {
   address = "https://vault.stevenincloud.online"
   token   = "hvs.FgsWrVWty4HsB021E2OblCFa" 
+}
+
+provider "aws" {
+  region = "eu-west-2"
 }
